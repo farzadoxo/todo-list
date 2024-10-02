bdd285abb3bfc0928915508c90d4b2bbf46f8271
farzadoxo <farzadebrahimi.oxo@gmail.com>
2024-10-02 18:26:56 +0330

    📓 Demo(test version)

---
+++ to do list/back-end/main.py
@@ -0,0 +1,23 @@
+
+
+
+class ToDoList:
+    """  Create a task , when its complete take a picture or video 
+         from completed task  """
+
+
+    def __init__(self,owner_name:str ="User"):
+        self.todo_list_owner_name = owner_name
+
+
+
+    def add_task(self , task:str):
+        if task != "":
+            todo_page = open(f"{self.todo_list_owner_name}.txt",mode='a')
+            todo_page.writelines(f"{task} \n")
+
+
+
+#test
+my_todo = ToDoList(owner_name="ali")
+my_todo.add_task(task="fuck sara")
