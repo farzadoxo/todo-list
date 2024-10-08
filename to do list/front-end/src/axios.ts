ffb89faee1b8071b3f8a9654fb045a9efbce7f36
farzadoxo <farzadebrahimi.oxo@gmail.com>
2024-10-08 20:35:11 +0330

    deleted

--- to do list/front-end/src/axios.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://localhost:3500', // Set your base URL
-});
-
-export default api;
--- to do list/front-end/src/types.ts
+++
@@ -1,7 +0,0 @@
-
-export interface TodoType {
-  id: number;
-  title: string;
-  completed: boolean;
-  dueDate: string | null;
-}
