2243264cf9020ce0afbbab57f3132bcd83fa00fe
farzadoxo <farzadebrahimi.oxo@gmail.com>
2024-10-20 20:07:44 +0330

    📄 Request body documention added

--- back-end/api.py
+++ back-end/api.py
@@ -13,7 +13,7 @@ class ReqBody(BaseModel):
     """this request body use for make a task or todo of a template 
     template : title = required  ------   completed = required  -------   dueDate = optional 
                string                       bool                           string or null"""
-    
+        
     title : str
     completed : bool = False
     dueDate : Optional[str] = None
