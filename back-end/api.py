d80dd36d6e4a5016c55a2183e121b58de3dec1b8
farzadoxo <farzadebrahimi.oxo@gmail.com>
2024-10-20 19:46:25 +0330

    🚩 new task api added

--- back-end/api.py
+++ back-end/api.py
@@ -1,14 +1,31 @@
 from main import Task
-from fastapi import FastAPI
+from fastapi import FastAPI , Path , Query , status , HTTPException
+from pydantic import BaseModel
+from typing import Optional
+import json
 
+
+# api contribute
 api = FastAPI()
 
 
-@api.get('')
-async def getAllTodos():
+class ReqBody(BaseModel):
+    title : str
+    completed : bool = False
+    dueDate : Optional[str] = None
+
+
+
+@api.get('/api/alltodos' , status_code=status.HTTP_200_OK)
+def all_todos():
     ...
 
 
-@api.get('')
-async def newTodo():
-    ...
+
+@api.post('/api/todos' , response_model=ReqBody , description="Task was successfully created (added)")
+def new_task(reqbody:ReqBody):
+    try:
+        with open("back-end/tests/todos.json",'a') as file:
+            json.dump(reqbody , file)
+    except HTTPException as error :
+        print(error)
