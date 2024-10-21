fd09d1900c3bc146cb154bca37e29ce68901d29c
farzadoxo <farzadebrahimi.oxo@gmail.com>
2024-10-21 20:42:02 +0330

    📑 Writing json file for database

--- back-end/api.py
+++ back-end/api.py
@@ -1,4 +1,4 @@
-from main import Task
+from random import randint
 from fastapi import FastAPI , Path , Query , status , HTTPException
 from pydantic import BaseModel
 from typing import Optional
@@ -13,7 +13,7 @@ class ReqBody(BaseModel):
     """this request body use for make a task or todo of a template 
     template : title = required  ------   completed = required  -------   dueDate = optional 
                string                       bool                           string or null"""
-        
+    
     title : str
     completed : bool = False
     dueDate : Optional[str] = None
@@ -26,10 +26,15 @@ def all_todos():
 
 
 
-@api.post('/api/todos' , response_model=ReqBody , description="Task was successfully created (added)")
+@api.post('/api/todos'  , description="Task was successfully created (added)")
 def new_task(reqbody:ReqBody):
+    print(reqbody)
     try:
-        with open("back-end/tests/todos.json",'a') as file:
-            json.dump(reqbody , file)
+        with open("back-end/__tests__/todos.json",'a') as file:
+            json.dump({
+                "id" : randint(1000034456,4564564563453525648734234234),
+                "title" : reqbody.title ,
+                "completed" : reqbody.completed ,
+                "dueDate" : reqbody.dueDate} , file)
     except HTTPException as error :
         print(error)
