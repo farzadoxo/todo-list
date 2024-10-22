0174eb16cf5e3e5c02e79abc508d87bdbee2a1d8
farzadoxo <farzadebrahimi.oxo@gmail.com>
2024-10-22 09:20:19 +0330

    🟩 api in progress ....

--- back-end/api.py
+++ back-end/api.py
@@ -1,16 +1,19 @@
 from random import randint
-from fastapi import FastAPI , Path , Query , status , HTTPException
+from PIL.Image import Image
+from fastapi import FastAPI , Path , Query , status , HTTPException 
 from pydantic import BaseModel
 from typing import Optional
 import sqlite3
 import json
 
 
-# api contribute
+# api instanse
 api = FastAPI()
 
 
-class ReqBody(BaseModel):
+
+
+class NewTaskReqBody(BaseModel):
     """this request body use for make a task or todo of a template 
     template : title = required  ------   completed = required  -------   dueDate = optional 
                string                       bool                           string or null"""
@@ -19,28 +22,67 @@ class ReqBody(BaseModel):
     completed : bool = False
     dueDate : Optional[str] = None
 
+
+
+
+class TaskCompletedReqBody(BaseModel):
+    """This request body use for upload a picture from completed task
+    template : task_id = required  ------   image = required
+                 string                       Image"""
+    
+    task_id : str
+    image : str
+
+
+
+# class ResMod(BaseModel):
+#     id : int
+#     title : str
+#     completed : bool = False
+#     dueDate : Optional[str] = None
+
+
+
 class DataBase:
+    """For use this class you should create a table called 'todos' 
+    with coloumns : id-title-completed-dueDate
+                    INTEGER-TEXT-BOOLEAN-TEXT
+    or run this code :
+    DataBase.cursor.execute("CREATE TABLE todos (id INTEGER , title TEXT , completed BOOLEAN , dueDate TEXT)")"""
+
     connection = sqlite3.connect("back-end/__tests__/database.db",check_same_thread=False)
     cursor = connection.cursor()
 
 
+
+
 @api.get('/api/alltodos' , status_code=status.HTTP_200_OK)
 def all_todos():
-    ...
+    DataBase.cursor.execute("SELECT * FROM todos")
+    items = DataBase.cursor.fetchall()
+    print(items)
 
 
 
-@api.post('/api/todos'  , description="Task was successfully created (added)")
-def new_task(reqbody:ReqBody):
-    print(reqbody)
+
+
+@api.post('/api/todos' , description="Task successfully created (added)" , status_code=status.HTTP_201_CREATED)
+def new_task(reqbody:NewTaskReqBody):
+    response_body = {
+        "id" : randint(1000034456,9564564347821289237),
+        "title" : reqbody.title,
+        "completed" : reqbody.completed,
+        "dueDate" : reqbody.dueDate
+    }
+
     try:
         DataBase.cursor.execute(f"INSERT INTO todos VALUES (?,?,?,?)",
-                                (randint(1000034456,9564564347821289237),reqbody.title,reqbody.completed,reqbody.dueDate))
+                                (response_body["id"],reqbody.title,reqbody.completed,reqbody.dueDate))
         DataBase.connection.commit()
+        return response_body
     except HTTPException as error :
         print(error)
 
-    
 
     # try:
     #     with open("back-end/__tests__/todos.json",'a') as file:
@@ -51,3 +93,15 @@ def new_task(reqbody:ReqBody):
     #             "dueDate" : reqbody.dueDate}] , file)
     # except HTTPException as error :
     #     print(error)
+
+
+
+@api.post('/api/upload' , status_code=status.HTTP_201_CREATED , description= "Task was successfully completed")
+def task_completed(reqbody:TaskCompletedReqBody):
+    DataBase.cursor.execute(f"SELECT * FROM todos WHERE id = {reqbody.task_id}")
+    item = DataBase.cursor.fetchone()
+    # --------------Update completed field to true (completed)----------------------------
+
+    DataBase.cursor.execute(f"UPDATE todos SET completed = true WHERE id = {reqbody.task_id}")
+    DataBase.connection.commit()
+    return reqbody
