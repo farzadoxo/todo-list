bc1c87614ef14ddef7a08ae11b0ace66065ab2ff
farzadoxo <farzadebrahimi.oxo@gmail.com>
2024-10-26 16:55:17 +0330

    🎉 Database madule setuped

---
+++ back-end/DATABASE/Db.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+class DataBase:
+    """For use this class you should create a table called 'todos' 
+    with coloumns : id-title-completed-dueDate
+                    INTEGER-TEXT-BOOLEAN-TEXT
+    or run this code :
+    DataBase.cursor.execute("CREATE TABLE todos (id INTEGER , title TEXT , completed BOOLEAN , dueDate TEXT)")"""
+
+    connection = sqlite3.connect("back-end/__tests__/database.db",check_same_thread=False)
+    cursor = connection.cursor()
