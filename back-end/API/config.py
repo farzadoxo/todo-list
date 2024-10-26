1569f8895c2de2b46b889bf62897c5ed8aa19712
farzadoxo <farzadebrahimi.oxo@gmail.com>
2024-10-26 17:20:29 +0330

    💯 Events and log system setuped

--- back-end/API/config.py
+++ back-end/API/config.py
@@ -1,6 +1,21 @@
 from API.api import router
+import datetime
 from fastapi import FastAPI 
 
 # api instanse
 Api = FastAPI()
 Api.include_router(router)
+
+
+# ----------- EVENTS ------------
+@Api.on_event("startup")
+def startup_log():
+
+    with open('back-end/API/API.log' ,mode='a') as log_file:
+        log_file.write(f"🟢 Server start At : {datetime.datetime.now()} \n")
+
+
+@Api.on_event("shutdown")
+def shutdown_log():
+    with open('back-end/API/API.log' , mode='a') as log_file:
+        log_file.write(f"🔴 Server shutdowned At : {datetime.datetime.now()} \n")
