cfc1270e842cae0bed224baef2fd3f9a016f4e20
farzadoxo <farzadebrahimi.oxo@gmail.com>
2024-10-26 17:20:55 +0330

    ⚠ Warning comment added

--- back-end/main.py
+++ back-end/main.py
@@ -2,3 +2,7 @@ import uvicorn
 
 if __name__ == "__main__":
     uvicorn.run(app="API.config:Api" , host='0.0.0.0' , port=8000 , reload=True)
+
+
+# ------------ WARNING --------------
+# change value of reload parametr to False when project ready for production !!!
