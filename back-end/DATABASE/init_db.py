6c44d4285086b6f8698d6c12822ab0bdf3b30d4a
farzadoxo <farzadebrahimi.oxo@gmail.com>
2024-11-14 20:36:35 +0330

    👤 Fullname coloumn added to users table

--- back-end/DATABASE/init_db.py
+++ back-end/DATABASE/init_db.py
@@ -30,8 +30,10 @@ def initialize_database(db_name: str = "database.db"):
         cursor = conn.cursor()
         cursor.execute("""
             CREATE TABLE IF NOT EXISTS users (
+                full_name TEXT NOT NULL ,
                 email TEXT NOT NULL PRIMARY KEY,
                 password TEXT NOT NULL
+
             )
         """) 
         conn.commit()
