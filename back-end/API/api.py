e56076228f78470d86ca06a9e3892f97797f2639
farzadoxo <farzadebrahimi.oxo@gmail.com>
2024-11-26 20:35:27 +0330

    signup endpoint (no test no debug no feature) dont tuch code

--- back-end/API/api.py
+++ back-end/API/api.py
@@ -1,7 +1,6 @@
 from API.models import NewTask, UpdateTask, Upload, SignUp, Login, UpdateAccountInfo
 from API.self_module import ResponseBody, IDGenerator
 from DATABASE.Db import DataBase
-from random import randint
 from fastapi import status, HTTPException, APIRouter, Response
 from fastapi.responses import JSONResponse
 
@@ -10,3 +9,46 @@ from fastapi.responses import JSONResponse
 router = APIRouter()
 
 
+# --------------------     User Section     --------------------
+
+@router.post(
+    '/api/account/signup',
+    status_code=status.HTTP_201_CREATED,
+    summary="Create Account",
+    description="Create account need a request body with 3 item (username , email , password)"
+)
+def signup(reqbody:SignUp , response:Response):
+    # fetch availeble user from database
+    DataBase.cursor.execute(
+        f"SELECT * FROM users WHERE email = '{reqbody.email}'"
+    )
+    user = DataBase.cursor.fetchone()
+
+    if user != None:
+        response.status_code = status.HTTP_409_CONFLICT
+        return "This email has already been used !"
+    else:
+        try:
+            if len(reqbody.password) >= 8:
+                # create a recorde (user signup)
+                DataBase.cursor.execute(
+                    f"INSERT INTO users VALUES (?,?,?)" , (reqbody.fullname,reqbody.email,reqbody.password)
+                )
+                DataBase.connection.commit()
+
+                DataBase.cursor.execute(
+                    f"SELECT * FROM users WHERE email = '{reqbody.email}'"
+                )
+                user = DataBase.cursor.fetchone()
+
+                # Returned 
+                return ResponseBody.UserResponseBody(user=user)
+            
+
+            else:
+                response.status_code = status.HTTP_400_BAD_REQUEST
+                return "The password must contain 8 characters or more and consist of numbers and letters"
+            
+        except Exception or HTTPException as error :
+            response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+            return f"ERROR >>> {error}"
