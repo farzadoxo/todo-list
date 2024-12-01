4c87db80305afd6be19401e62a852a1ffa6e1734
farzadoxo <farzadebrahimi.oxo@gmail.com>
2024-12-01 22:44:48 +0330

    log system added (not complete)

--- back-end/API/self_module.py
+++ back-end/API/self_module.py
@@ -3,6 +3,8 @@ from DATABASE.Db import DataBase
 
 
 class ResponseBody:
+    """Make a response body (.json (dict)) using a ready
+       template and inputs """
 
     def UserResponseBody(user: tuple):
         response_body = {
@@ -13,7 +15,7 @@ class ResponseBody:
 
         return response_body
 
-    def TaskResponseBody(task: tuple):
+    def TodoResponseBody(task: tuple):
         response_body = {
             "id": task[0],
             "title": task[1],
@@ -24,19 +26,46 @@ class ResponseBody:
         return response_body
 
 
-class IDGenerator:
-    def generate_id():
-        id = randint(1000034456, 9564564347821289237)
+# class IDGenerator:
+#     def generate_id():
+#         id = randint(1000034456, 9564564347821289237)
 
-        DataBase.cursor.execute("SELECT id from todos")
-        active_ids = DataBase.cursor.fetchall()
+#         DataBase.cursor.execute("SELECT id from todos")
+#         active_ids = DataBase.cursor.fetchall()
 
-        for id_countaner in active_ids:
-            if id_countaner == id:
-                id = randint(1000034456, 9564564347821289237)
-                return id
-            else:
-                return id
+#         for id_countaner in active_ids:
+#             if id_countaner == id:
+#                 id = randint(1000034456, 9564564347821289237)
+#                 return id
+#             else:
+#                 return id
 
 
+class LogSystem:
+    """Log all things happend in API and DATABASE
+       Log users acctivity and todos status"""
 
+    class TodoLog:
+        """Log todos status (users.log)"""
+
+        def on_todo_add():
+            ...
+
+        def on_todo_remove():
+            ...
+        
+        def on_todo_update():
+            ...
+
+
+    class UserLog:
+        """Log user status (todos.log)"""
+
+        def on_user_signup():
+            ...
+
+        def on_user_login():
+            ...
+
+        def on_account_deleted():
+            ...
