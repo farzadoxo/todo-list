85e9416cd88b5b49e5ddc9c49ab002e469a1a0c1
farzadoxo <farzadebrahimi.oxo@gmail.com>
2024-12-11 09:37:27 +0330

    Log system bug fixed

--- back-end/API/config.py
+++ back-end/API/config.py
@@ -2,14 +2,9 @@ import logging
 from API.api import router
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from API.self_module import LoggerSetup
 
-# Configure logging
-logging.basicConfig(
-    filename="./API.log",
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s",
-    encoding="utf-8",
-)
+
 
 # API instance
 Api = FastAPI()
@@ -29,9 +24,9 @@ Api.add_middleware(
 # ----------- EVENTS ------------
 @Api.on_event("startup")
 def startup_log():
-    logging.info("🟢 Server started")
+    LoggerSetup.server_logger.info("🟢 Server started")
 
 
 @Api.on_event("shutdown")
 def shutdown_log():
-    logging.info("🔴 Server shutdown")
+    LoggerSetup.server_logger.info("🔴 Server shutdown")
