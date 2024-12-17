ffa4ceb70b8ecdd26b7a2ef9f61bba2ccce65fc7
farzadoxo <farzadebrahimi.oxo@gmail.com>
2024-12-17 12:25:01 +0330

    models converted to schemas

---
+++ back-end/API/schemas.py
@@ -0,0 +1,57 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+
+class NewTodo(BaseModel):
+    """this request body use for make a task or todo of a template"""
+    
+    title : str
+    completed : bool = False
+    dueDate : Optional[str] = None
+    priority : Optional[str] = None
+
+
+
+
+# class ResMod(BaseModel):
+#     id : int
+#     title : str
+#     completed : bool = False
+#     dueDate : Optional[str] = None
+
+
+
+
+class UpdateTodo(BaseModel):
+    """This request body use for deleting a task from database"""
+
+    title : Optional[str] = None
+    completed : Optional[bool] = None
+    dueDate : Optional[str] = None
+    priority : Optional[str] = None
+
+
+
+class SignUp(BaseModel):
+    """This request body use for signup to service"""
+    
+    full_name : str
+    email : str
+    password : str
+
+
+
+class Login(BaseModel):
+    """This request body use for login to account"""
+
+    email : str
+    password : str
+
+
+
+class UpdateAccountInfo(BaseModel):
+    """This request bosy use fot update or change account info"""
+    
+    new_email : Optional[str] = None
+    new_password : Optional[str] = None
