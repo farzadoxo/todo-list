f4709dce193495a18095f4751ca696922fc82add
farzadoxo <farzadebrahimi.oxo@gmail.com>
2025-01-12 12:33:21 +0330

    some setup added

--- back-end/main.py
+++ back-end/main.py
@@ -1,8 +1,11 @@
 import uvicorn
 from API.self_module import setup_directories
+from DATABASE.init_db import initialize_database
 
 if __name__ == "__main__":
-     # Setup directories and assets
+    # Setup database file and tables
+    initialize_database()
+    # Setup directories and assets
     setup_directories()
     # Run API server
     uvicorn.run(app="API.config:Api", host="0.0.0.0", port=8000, reload=True)
