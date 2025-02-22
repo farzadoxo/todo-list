5036237ad340eecc744e63e1b853c09f7cf0dd39
farzadoxo <farzadebrahimi.oxo@gmail.com>
2025-02-22 20:50:44 +0330

    models added

---
+++ back-end/Models/Todo.py
@@ -0,0 +1,9 @@
+class Todo:
+    def __init__(self,id,owner,title,completed,dueDate,priority,image_url):
+        self.id = id
+        self.owner = owner
+        self.title = title
+        self.completed = completed
+        self.dueDate = dueDate
+        self.priority = priority
+        self.image_url = image_url
---
+++ back-end/Models/User.py
@@ -0,0 +1,6 @@
+class User:
+    def __init__(self,fullname,email,password,avatar_url):
+        self.fullname = fullname
+        self.email = email
+        self.password = password
+        self.avatar_url = avatar_url
