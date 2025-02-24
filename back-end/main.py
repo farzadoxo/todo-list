e6f9942790b489f56ac43db2005b94951bacf580
farzadoxo <farzadebrahimi.oxo@gmail.com>
2025-02-24 10:35:30 +0330

    directory creator added

--- back-end/main.py
+++ back-end/main.py
@@ -1,15 +1,22 @@
 import uvicorn
-from API.self_module import setup_directories
+import os
 from DATABASE.init_db import initialize_database
 
 if __name__ == "__main__":
+    
     # Setup database file and tables
     try:
         initialize_database()
     except:
         pass
+
     # Setup directories and assets
-    setup_directories()
+    try:
+        os.makedirs(os.path.join('ASSETS' , 'Profiles'),exist_ok=True)
+        os.makedirs(os.path.join('ASSETS' , 'Todos'),exist_ok=True)
+    except:
+        pass
+
     # Run API server
     uvicorn.run(app="API.config:Api", host="0.0.0.0", port=8000, reload=True)
 
